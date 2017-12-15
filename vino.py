63f31998b115a09fd310eff36431d29fe02d0a73
giant-steps <chrisdklocke@gmail.com>
2017-12-15 18:59:35 -0500

    progress on vino

--- vino.py
+++ vino.py
@@ -12,11 +12,17 @@ def main():
 
     reader = csv.reader(open(sys.argv[1]), delimiter="\t")
     inp = list(reader)
-    points = np.array(inp).astype("float")
+    points = np.array(inp).astype("str")      ## must change to string for now
     ### data1.csv, for now: 0:9 : PassID,Surv,pclass,sex,age,sibsp,parch,fare,cabin,embarked
         #remember: row,column
 
-    #print(str(points))     ############
+    print(points)     ############
+
+    ## vino1.csv fields:    points, title, variety, description, country, province, region1, region2,
+    ##                          winery, designation, price, taster name, taster twitter handle
+
+    ## order in file: number, country, description, designation, points, price, prov, reg1, reg2,
+            ## variety, winery
 
     aa = points[:,3]
     bb = points[:,1]      ### for single variable OLS, use this to choose independent variable
@@ -29,7 +35,14 @@ def main():
     model = sm.OLS(y, X).fit()
     predictions = model.predict(X)
 
-    print(model.summary())
+    #print(model.summary())
+
+    """
+    ypred = model.predict(X)
+
+    """
+
+
 
 # RUN MAIN FUNCTION
 if __name__ == "__main__":
